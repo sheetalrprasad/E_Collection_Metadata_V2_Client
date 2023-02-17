c3b9749af7ad35a3b7eee4894d61535b09ff03f4
Sheetal Rani Prasad <sprasad@sdsu.edu>
2023-02-17 11:25:36 -0800

    Login Page

--- src/Login/LoginApp.js
+++ src/Login/LoginApp.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useRef, useState, useEffect } from "react";
+import "./LoginApp.css";
 
 
 
@@ -8,8 +9,8 @@ const LoginApp = () =>{
     const userRef = useRef();
     const errRef = useRef();
 
-    const [user, setUser] = useState();
-    const [pwd, setPwd] = useState();
+    const [user, setUser] = useState('');
+    const [pwd, setPwd] = useState('');
     const [errMsg, setErrMsg] = useState();
     const [success, setSuccess] = useState(false);
 
@@ -22,60 +23,64 @@ const LoginApp = () =>{
     },[user, pwd])
 
     const handleSubmit = async (e) =>{
-        e.preventdefault();
-        //call api
-        console.log(user, pwd);
-        setUser('');
-        setPwd(' ');
+
         setSuccess(true);
+        console.log(user, pwd);
+        //call api
+        setUser('');
+        setPwd('');
+        
+        e.preventDefault();
+        
     }
 
     return (
         <div>
-            { success? ( 
+            { success ? ( 
                 <section>
                     <h1>You are logged In.</h1>
                     <p><a href="/"> Home</a></p>
                 </section>
-                ): (
-                    <div className="form-group">
-                        <section>
-                            <p ref={errRef} className={errMsg? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
-                            <h1>Sign In</h1>
-                            <form onSubmit={handleSubmit}>
-                                
-                                <label htmlFor="username">Username:</label>
-                                <input 
-                                    type="text" 
-                                    id="username"
-                                    className="form-control" 
-                                    ref={userRef} 
-                                    autoComplete="off" 
-                                    onChange={(e) => setUser(e.target.value)}
-                                    value={user}
-                                    required
-                                />
+                ) : (
+                        <div className="form-group login">
+                            <section>
+                                <p ref={errRef} className={errMsg? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+                                <h1>Sign In</h1>
+                                <form onSubmit={(e) => handleSubmit(e) }>
+                                    
+                                    <label htmlFor="username">Username:</label>
+                                    <input 
+                                        type="text" 
+                                        id="username"
+                                        className="form-control" 
+                                        ref={userRef} 
+                                        autoComplete="off" 
+                                        onChange={(e) => setUser(e.target.value)}
+                                        value={user}
+                                        required
+                                    />
 
-                                <label htmlFor="password">Password:</label>
-                                <input 
-                                    type="password" 
-                                    id="password" 
-                                    className="form-control"
-                                    onChange={(e) => setPwd(e.target.value)}
-                                    value={pwd}
-                                    required
-                                />
+                                    <label htmlFor="password">Password:</label>
+                                    <input 
+                                        type="password" 
+                                        id="password" 
+                                        className="form-control"
+                                        onChange={(e) => setPwd(e.target.value)}
+                                        value={pwd}
+                                        required
+                                    />
 
-                                <button type="submit" className="btn btn-primary">Sign In</button>
+                                    <button type="submit" className="btn btn-primary login-button">Sign In</button>
 
-                                <p>
-                                    Need an Account?<br/>
-                                    Reach out to admin for creation of new account.
-                                </p>
-                            </form>
-                        </section>
-                    </div>
-            )}
+                                    <p>
+                                        Need an Account?<br/>
+                                        Reach out to admin for creation of new account.
+                                    </p>
+                                </form>
+                            </section>
+                        </div>
+                    )
+                }
         </div>
     )
     
